6fd68b3724d1f4eb37e4265661663bc0ea7d8379
Orkun Berk Yuzbasioglu <orkunberk@hotmail.com>
2020-08-01 14:43:14 +0300

    Iniital Commit

---
+++ courses/views.py
@@ -0,0 +1,10 @@
+# Create your views here.
+
+from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Course
+from .serializers import CourseSerializer
+
+class CourseView(viewsets.ModelViewSet):
+	queryset = Course.objects.all()
+	serializer_class = CourseSerializer
